55bdad77a2f1a78ae7248e82e11366aaa2f5c68e
Kevin Hao <kevinhao3300@yahoo.com>
2020-05-18 16:24:41 -0500

    graphs

--- graph.py
+++ graph.py
@@ -2,20 +2,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # with open('test_log/100smalljpg.txt','r') as f:
-# 	plt.figure()
 # 	x = []
 # 	arr = []
 # 	for line in f:
 # 		epoch, acc = line.split()
 # 		arr.append(float(acc))
 # 		x.append(int(epoch))
-# 	print(x)
 # 	print(arr)
-# 	plt.plot(x,arr)
-	# plt.show()
+# 	fig = plt.figure()
+# 	ax1 = fig.add_subplot()
+# 	ax1.set_title('Test Set Loss with 100 Demonstrations')
+# 	ax1.set_ylabel('cross entropy loss')
+# 	ax1.set_xlabel('epoch #')
 
-acc = np.load('acc.npy')
+# 	ax1.plot(x,arr, lw=2)
+
+# 	plt.show()
+
+acc = np.load('sureness.npy')
+acc /= np.max(acc)
 x = list(range(0,100))
-plt.figure()
-plt.plot(x,acc)
+# # plt.figure()
+# # plt.xlabel = 'cross entropy loss'
+
+# # plt.plot(x,acc)
+# # plt.show()
+
+
+fig = plt.figure()
+ax1 = fig.add_subplot()
+ax1.set_title('Sureness with 100 Demonstrations')
+ax1.set_ylabel('cross entropy loss')
+ax1.set_xlabel('epoch #')
+
+ax1.plot(x,acc, lw=2)
+
 plt.show()
